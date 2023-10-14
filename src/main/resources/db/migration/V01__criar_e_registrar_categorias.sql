8d8f9fff69e3dc7b1f8c1899b0cdd4fd83f51e13
Anthony Macedo <anthonymacedo@live.com>
2023-10-14 17:33:21 -0300

    SQL

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -1,9 +1,9 @@
+
 CREATE TABLE IF NOT EXISTS sch_sistema.categoria (
     id bigserial NOT NULL,
     descricao character varying(60) NOT NULL,
     CONSTRAINT categoria_pkey PRIMARY KEY (id)
 );
-
 
 INSERT INTO sch_sistema.categoria(descricao) VALUES ('Farmácia');
 INSERT INTO sch_sistema.categoria(descricao) VALUES ('Supermercado');
